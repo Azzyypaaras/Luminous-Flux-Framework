a40864eb31a8fb08a41576173040720f50ac9f89
Azazelthedemonlord <spectreneo@gmail.com>
2020-07-23 00:56:46 -0500

    Woops

--- src/main/java/azzy/fabric/lff/LuminousFlux.java
+++ src/main/java/azzy/fabric/lff/LuminousFlux.java
@@ -9,38 +9,10 @@ import java.util.PriorityQueue;
 
 public class LuminousFlux implements ModInitializer {
 
-	Comparator<Test> sorter = Comparator.comparing(Test::getPriority);
-	PriorityQueue<Test> queue = new PriorityQueue<>(16, sorter);
 	Logger LFFlog = LogManager.getLogger("Luminous Flux Framework");
 
-	@Override public void onInitialize() {
+	@Override
+	public void onInitialize() {
 
-		queue.add(new Test(1));
-		queue.add(new Test(2));
-		queue.add(new Test(3));
-		queue.add(new Test(4));
-		queue.add(new Test(5));
-
-		while(!queue.isEmpty()){
-			LFFlog.error(queue.poll().priority);
-		}
-	}
-
-	private class Test implements Comparable<Test>{
-
-		int priority;
-
-		public Test(int priority){
-			this.priority = priority;
-		}
-
-		public int getPriority() {
-			return priority;
-		}
-
-		@Override
-		public int compareTo(Test o) {
-			return priority;
-		}
 	}
 }
